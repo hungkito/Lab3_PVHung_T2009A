6011d160920ba467e989897de3177ad28b8e16b7
hungkito <hungpvth2008007@fpt.edu.vn>
2021-06-11 20:53:14 +0700

    first commit

---
+++ Part4_ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Part4_ex2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee testEmployee = new Employee("Phung", "Hung", "Lao Cai", 10, 10000000);
+            Console.WriteLine(testEmployee.ToString());
+            Console.WriteLine(testEmployee.calculateTheBonus(2));
+        }
+    }
+}
